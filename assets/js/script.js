b0d5cf2bae0351317578aaf6239e3832dac61120
MuhammadUsman <imdevusman@gmail.com>
2024-06-14 19:42:03 +0500

    main

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,8 +32,3 @@ function toggleSidebar() {
 
   let table = new DataTable('#example');
  
-  table.on('click', 'tbody tr', function () {
-      let data = table.row(this).data();
-   
-      alert('You clicked on ' + data[0] + "'s row");
-  });
