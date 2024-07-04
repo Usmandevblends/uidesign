b1e3541cea35189e9dd7fae92442517986c8a0f6
MuhammadUsman <imdevusman@gmail.com>
2024-07-04 16:33:15 +0500

    owlcarousel

--- assets/js/script.js
+++ assets/js/script.js
@@ -83,3 +83,25 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+// owl carosle
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  responsiveClass:true,
+  responsive:{
+      0:{
+          items:1,
+          nav:true
+      },
+      600:{
+          items:3,
+          nav:false
+      },
+      1000:{
+          items:5,
+          nav:true,
+          loop:false
+      }
+  }
+})
